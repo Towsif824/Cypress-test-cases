d44533ace7eb454248cc4eca91146d2fcc945f9f
Towsif824 <tawsif824@gmail.com>
2022-08-22 13:18:06 +0600

    Updated category creation script

--- cypress/e2e/create_category.spec.cy.js
+++ cypress/e2e/create_category.spec.cy.js
@@ -1,16 +1,24 @@
 describe('Category Test Suit', ()=>{
     it('view Categories', function(){
       cy.visit('https://app.test.pennyperfect.ca/auth')
-      cy.contains('Email').type('towsif.sandbox@gmail.com')//provide your user's email address
-      cy.contains('Password').type('123456')// provide your user's passowrd
+      cy.contains('Enter your email').type('towsif.sandbox@gmail.com')
+      cy.contains('Password').type('123456')
       cy.get('.q-form > .q-card__actions > .q-btn > .q-btn__wrapper > .q-btn__content > .block').click()
-      cy.get(':nth-child(7) > .q-expansion-item__container > div.q-item > .q-item__section--main > .q-item__label').click()//selecting CATALOG
-      cy.get('[href="/categories"] > .q-item__section > .q-item__label').click() //selecting Categoris & products 
+      cy.wait(1000)
+
+      cy.xpath('//div[normalize-space()="CATALOG"]').click()//selecting CATALOG
+      cy.xpath('//div[contains(text(),"Categories & Products")]').click() //selecting Categoris & products 
     })
 
     it('Create Category', function(){
       cy.xpath('//i[normalize-space()="add"]').click()
-      cy.contains('Name').type('Test')
+      cy.contains('Name').type('Laptop')
+      cy.xpath('//span[contains(text(),"Create")]').click()
+    })
+
+    it('Create Category', function(){
+      cy.xpath('//i[normalize-space()="add"]').click()
+      cy.contains('Name').type('Mobile')
       cy.xpath('//span[contains(text(),"Create")]').click()
     })
 })
