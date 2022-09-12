72a46f9be4a5d479c7d7704c4652b6263f78e082
Towsif824 <tawsif824@gmail.com>
2022-09-12 13:53:38 +0600

    updated the env

--- cypress/e2e/create_category.spec.cy.js
+++ cypress/e2e/create_category.spec.cy.js
@@ -1,6 +1,6 @@
 describe('Category Test Suit', ()=>{
     it('view Categories', function(){
-      cy.visit('https://app.test.pennyperfect.ca/auth')
+      cy.visit(`${Cypress.env('pennyperfect_baseurl')}`)
       cy.contains('Enter your email').type('towsif.sandbox@gmail.com')
       cy.contains('Password').type('123456')
       cy.get('.q-form > .q-card__actions > .q-btn > .q-btn__wrapper > .q-btn__content > .block').click()
