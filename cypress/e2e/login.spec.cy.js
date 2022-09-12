e3a11fed5f97b2ed82838f5dbbae44464ae9015e
Towsif824 <tawsif824@gmail.com>
2022-09-12 13:48:48 +0600

    updated the env

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,20 +1,19 @@
 describe('Login Test Suit', ()=>{
   it('Login', function(){
-    cy.wait(4000)
     cy.visit(`${Cypress.env('pennyperfect_baseurl')}`)
     cy.contains('Enter your email').type('towsif.sandbox@gmail.com')
     cy.contains('Password').type('123456')
     cy.get('.q-form > .q-card__actions > .q-btn > .q-btn__wrapper > .q-btn__content > .block').click()
 
-    cy.get('.text-uppercase').invoke('text').then((text)=>{
+    /*cy.get('.text-uppercase').invoke('text').then((text)=>{
       expect(text).to.eq('Dashboard')
-  })
+    })*/
 
     cy.url().should('include', '/dashboard')
   })
 
   it('login with invalid email', function(){
-    cy.visit('https://app.test.pennyperfect.ca/auth')
+    cy.visit(`${Cypress.env('pennyperfect_baseurl')}`)
     cy.contains('Enter your email').type('towsif.sanbox@gmail.com')
     cy.contains('Password').type('123456')
     cy.get('.q-form > .q-card__actions > .q-btn > .q-btn__wrapper > .q-btn__content > .block').click()
@@ -25,7 +24,7 @@ describe('Login Test Suit', ()=>{
   })
 
   it('login with invalid password', function(){
-    cy.visit('https://app.test.pennyperfect.ca/auth')
+    cy.visit(`${Cypress.env('pennyperfect_baseurl')}`)
     cy.contains('Enter your email').type('towsif.sandbox@gmail.com')
     cy.contains('Password').type('145996')
     cy.get('.q-form > .q-card__actions > .q-btn > .q-btn__wrapper > .q-btn__content > .block').click()
